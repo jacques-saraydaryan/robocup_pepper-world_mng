50348b4b7a36d38e6d14c169fbbae3f7de61a5f5
jsaraydaryan <jsaraydaryan@cmp.com>
2021-07-02 11:41:35 +0200

    fix case when too many categories, add cluster visulalization, update test case file

--- 2Dto3DEntity/scripts/process/EntityMergeMng.py
+++ 2Dto3DEntity/scripts/process/EntityMergeMng.py
@@ -3,6 +3,8 @@ from common.FifoEntity import FifoEntity
 from sklearn.cluster import DBSCAN
 from sklearn.preprocessing import StandardScaler,OneHotEncoder
 from sklearn.compose import ColumnTransformer
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
 class EntityMergeMng:
@@ -10,11 +12,13 @@ class EntityMergeMng:
     DBSCAN_EPS_VALUE = 0.3
     DBSAN_MIN_SAMPLES = 10
     DBSAN_NO_CLUSTER_LABEL = -1
-    def __init__(self, buffer_size, ttl,dbscan_eps_value=0.3,dbscan_min_samples = 1):
+    DISPLAY_PROCESS = False
+    def __init__(self, buffer_size, ttl,dbscan_eps_value=0.3,dbscan_min_samples = 1, display_process=False):
         self._buffer_size = buffer_size
         self.DBSCAN_EPS_VALUE=dbscan_eps_value
         self.DBSAN_MIN_SAMPLES=dbscan_min_samples
         self._fifo_entity=FifoEntity(buffer_size, ttl,ttl)
+        self.DISPLAY_PROCESS=display_process
 
     def add_entity(self, entity, x, y ,z,category):
          self._fifo_entity.add_entity(entity, x, y ,z,category)
@@ -62,15 +66,40 @@ class EntityMergeMng:
         #FIXME understood why such thing occurred
         # e.g of bad list : [['0.478925979158', '1.72041366053', '0.338162953405', 'pitcher'], ['0.0', '0.0', '0.0', 'legoduplo'], ['0.839994215872', '1.39460499135', '0.220023868548', 'pitcher'], ['1.43720197384', '1.74155567275', '0.515741799004', 'pitcher'], ['0.0', '0.0', '0.0', 'legoduplo'], ['3.10619888936', '3.18450739502', '0.000960778115468', 'coloredwoodblock'], ['3.13818368201', '3.49937216121', '0.000840265950091', 'coloredwoodblock'], ['1.83351236984', '5.08687828065', '0.861111093251', 'pottedmeat'], ['1.51749251286', '4.82177696414', '0.748751834135', 'sugar'], ['1.13746645355', '5.0060511129', '0.895427254161', 'pottedmeat'], ['0.858438474374', '4.62266843231', '0.961419217456', 'mustard'], ['0.691892589903', '4.95993932903', '0.862440519674', 'pottedmeat'], ['0.406098451559', '4.49834130458', '0.895938725193', 'mustard'], ['0.12812369009', '3.89425594954', '0.534817657272', 'legoduplo'], ['-0.27551997642', '4.73159381279', '0.600139205634', 'gelatin'], ['-0.583274241587', '4.63919945311', '0.600312986393', 'pottedmeat'], ['-0.725278029395', '4.54877949488', '0.530165151059', 'mustard'], ['-0.930979771043', '4.17856720669', '0.298943007401', 'lemon'], ['0.0', '0.0', '0.0', 'gelatin'], ['0.0', '0.0', '0.0', 'pottedmeat'], ['0.324886017928', '4.84101028613', '0.482104467322', 'tuna'], ['0.0', '0.0', '0.0', 'mustard'], ['-0.15773771022', '4.70698538593', '0.121250146812', 'strawberry'], ['-0.512578612166', '4.60848387188', '0.399463721501', 'lemon'], ['0.0', '0.0', '0.0', 'mustard'], ['0.113144736685', '4.78243481513', '0.233974019509', 'legoduplo'], ['-0.327620506584', '4.66011023016', '0.0769996761656', 'legoduplo'], ['0.438153607422', '4.8728203368', '0.347743386239', 'legoduplo'], ['-0.27024133607', '5.62588573594', '0.181816546111', 'legoduplo'], ['0.3229969383', '4.84099333885', '0.350572543268', 'legoduplo'], ['-0.655597595775', '3.44368888534', '0.390338001539', 'pitcher'], ['-0.240414552757', '5.08268149248', '0.311368182172', 'pitcher'], ['2.66803955614', '4.44257795974', '0.500825322086', 'legoduplo'], ['3.61838012352', '4.3264846375', '0.4755975006', 'cup'], ['3.62616664945', '3.72937549471', '0.000888229785055', 'legoduplo']]
         except ValueError as e:
-            print('ERROR during converting list to numpy array and OneHotEncoder, '+str(e))
+            print('ERROR during converting list to numpy array and OneHotEncoder, may be to many categories, '+str(e))
             print('coordlist_cropped: ')
             print(coordlist_cropped)
-            return {}
+            try:
+                print("------RECOVERY MODE: Compute individual cluster for each category-----")
+                current_cluster_key_index=0
+                resulted_cluster={}
+                data_array=np.array(coordlist_cropped)
+                unique = np.unique(data_array[:,3], return_counts=False)
+                for cat in unique:
+                    print("------%s-----"%cat)
+                    current_array_indices=np.where(data_array[:,3]==cat)
+                    #FIXME complete and extract only value of the same label
+                    #print(current_array_indices)
+                    short_array=np.array(data_array[current_array_indices][:,:3],dtype=float)
+                    #print(short_array)
+                    current_cat_clusters=self.computeClusters(short_array, coordlist,entityList,index_converter_map=current_array_indices)
+                    #fill the resulted cluster of each categories with all clusters detected on the given category
+                    for key in current_cat_clusters:
+                        resulted_cluster[current_cluster_key_index]=current_cat_clusters[key]
+                        current_cluster_key_index+=1
+                return resulted_cluster
 
+            except Exception as e:
+                print(e)
+                self._fifo_entity.reset()
+                return {}
 
         #Standardize features by removing the mean and scaling to unit variance
         #X = StandardScaler().fit_transform(coord_list)
+        return self.computeClusters(data, coordlist,entityList)
 
+
+    def computeClusters(self, data, coordlist,entityList, index_converter_map={}):
         # Clusterized data using DBSCAN with x,y,z,category (one dimension per category --> OneHotEncoder)
         try:
             db = DBSCAN(eps=self.DBSCAN_EPS_VALUE, min_samples=self.DBSAN_MIN_SAMPLES).fit(data)
@@ -79,6 +108,56 @@ class EntityMergeMng:
             return {}
         #print("----------------------- Current DB ------------------")
         #print(db.labels_)
+
+        if self.DISPLAY_PROCESS:
+            #TODO display cluster
+            #build xy matrix
+            # matrix size
+            matrix_elt_number = data.shape[1]
+            data_x=data[:,matrix_elt_number-3]
+            data_y=data[:,matrix_elt_number-2]
+            data_z=data[:,matrix_elt_number-1]
+            data_xyz=np.column_stack((data_x,data_y,data_z))
+
+            core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+            core_samples_mask[db.core_sample_indices_] = True
+            labels=db.labels_
+            # Number of clusters in labels, ignoring noise if present.
+            n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+            #
+            ## Black removed and is used for noise instead.
+            unique_labels = set(labels)
+            colors = [plt.cm.Spectral(each)
+                    for each in np.linspace(0, 1, len(unique_labels))]
+                    #check color in hexa for plot3d https://stackoverflow.com/questions/33596491/extract-matplotlib-colormap-in-hex-format
+            colors_hexa=[]
+            for c in colors:
+                #convert to RBG format 255
+                c_rgb=(int(round(c[0]*float(255))),int(round(c[1]*float(255))),int(round(c[2]*float(255))))
+                #converto hexa
+                current_hexa='#%02x%02x%02x' % c_rgb
+                colors_hexa.append(current_hexa)
+
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+
+            for k, col in zip(unique_labels, colors_hexa):
+                if k == -1:
+            #        # Black used for noise.
+                    #col = [0, 0, 0, 1]
+                    col='#000000'
+                class_member_mask = (labels == k)
+                xyz = data_xyz[class_member_mask & core_samples_mask]
+                #plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+                #        markeredgecolor='k', markersize=14)
+                ax.scatter(xyz[:, 0], xyz[:, 1], xyz[:, 2],s=50, c=col, marker='^')
+
+                xyz = data_xyz[class_member_mask & ~core_samples_mask]
+                #plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+                #        markeredgecolor='k', markersize=6)
+                ax.scatter(xyz[:, 0], xyz[:, 1], xyz[:, 2], c=col, marker='o')
+            plt.title('Estimated number of clusters: %d' % n_clusters_)
+            plt.show()
 
         # Collect coord info from cluster
         cluster_to_key={}
@@ -98,10 +177,18 @@ class EntityMergeMng:
             c_z=0
             clusters[key_label]={'entity_list':[], 'c':(0,0,0)}
             for coord_index in cluster_to_key[key_label]:
-                c_x = c_x + coordlist[coord_index][0]
-                c_y = c_y + coordlist[coord_index][1]
-                c_z = c_z + coordlist[coord_index][2]
-                clusters[key_label]['entity_list'].append(entityList[coord_index])
+                if index_converter_map == {}:
+                    c_x = c_x + float(coordlist[coord_index][0])
+                    c_y = c_y + float(coordlist[coord_index][1])
+                    c_z = c_z + float(coordlist[coord_index][2])
+                    clusters[key_label]['entity_list'].append(entityList[coord_index])
+                else:
+                    remapped_index=index_converter_map[0][coord_index]
+                    c_x = c_x + float(coordlist[remapped_index][0])
+                    c_y = c_y + float(coordlist[remapped_index][1])
+                    c_z = c_z + float(coordlist[remapped_index][2])
+                    clusters[key_label]['entity_list'].append(entityList[remapped_index])
+
             avg_c_x=c_x/cluster_size
             avg_c_y=c_y/cluster_size
             avg_c_z=c_z/cluster_size
--- 2Dto3DEntity/scripts/test/test_EntityMergeMng.py
+++ 2Dto3DEntity/scripts/test/test_EntityMergeMng.py
@@ -6,6 +6,12 @@ print(sys.path)
 sys.path.append(os.path.dirname(__file__)+'/../')
 print(sys.path)
 from process.EntityMergeMng import EntityMergeMng
+import rospy
+
+class Entity:
+    def __init__(self,uuid,label="fakeLabel"):
+        self.uuid=uuid
+        self.label=label
 
 class TestEntityMergeMng(unittest.TestCase):
 
@@ -15,97 +21,97 @@ class TestEntityMergeMng(unittest.TestCase):
 
     def test_add_one_entity(self):
         entity_mng = EntityMergeMng(10,10)
-        entity_mng.add_entity("blabla",5,5,5,"car")
-        self.assertEqual(1,entity_mng.buffer_entity_list[entity_mng._current_buffer_indice].size())
+        entity_mng.add_entity(Entity("blabla"),5,5,5,"car")
+        self.assertEqual(1,len(entity_mng._fifo_entity._fifo_entity.queue))
 
     def test_add_entity_exceeded(self):
         entity_mng = EntityMergeMng(5,5)
         for i in range(0,6):
             try:
-                entity_mng.add_entity("blabla"+str(i),i,5,5,"car")
+                entity_mng.add_entity(Entity("blabla"+str(i)),i,5,5,"car")
             except Exception as e:
                 print(e)
-                self.assertEqual(5,i)
+                self.assertTrue(True)
                 return
-        self.assertTrue(False)
+        self.assertEqual("blabla1",entity_mng._fifo_entity._fifo_entity.get().uuid)
 
 
     def test_process_buffer_3clusters(self):
         entity_mng = EntityMergeMng(10,5,dbscan_eps_value=1,dbscan_min_samples=3)
         #cluster 1
-        entity_mng.add_entity("blabla1",0,0,5,"toto")
-        entity_mng.add_entity("blabla2",0,0.5,5,"toto")
-        entity_mng.add_entity("blabla3",0.5,0,5,"toto")
-        entity_mng.add_entity("blabla4",0.5,0,4.3,"toto")
+        entity_mng.add_entity(Entity("blabla1"),0,0,5,"toto")
+        entity_mng.add_entity(Entity("blabla2"),0,0.5,5,"toto")
+        entity_mng.add_entity(Entity("blabla3"),0.5,0,5,"toto")
+        entity_mng.add_entity(Entity("blabla4"),0.5,0,4.3,"toto")
 
-        entity_mng.add_entity("blabla5",3,2,0,"car")
-        entity_mng.add_entity("blabla6",2.5,1.5,0,"car")
-        entity_mng.add_entity("blabla7",3.2,1.75,0,"car")
+        entity_mng.add_entity(Entity("blabla5"),3,2,0,"car")
+        entity_mng.add_entity(Entity("blabla6"),2.5,1.5,0,"car")
+        entity_mng.add_entity(Entity("blabla7"),3.2,1.75,0,"car")
         
         #cluster 3
-        entity_mng.add_entity("blabla8",10,0.3,5,"bottle")
-        entity_mng.add_entity("blabla9",9.7,0,5,"bottle")
-        entity_mng.add_entity("blabla10",10.3,0.3,5,"bottle")
+        entity_mng.add_entity(Entity("blabla8"),10,0.3,5,"bottle")
+        entity_mng.add_entity(Entity("blabla9"),9.7,0,5,"bottle")
+        entity_mng.add_entity(Entity("blabla10"),10.3,0.3,5,"bottle")
 
-        clusters=entity_mng._process_buffer()
+        clusters=entity_mng.process_buffer()
         self.assertEqual(3,len(clusters))
 
     def test_process_buffer_2clusters(self):
-        entity_mng = EntityMergeMng(10,5,dbscan_eps_value=1.0,dbscan_min_samples=3)
+        entity_mng = EntityMergeMng(10,5,dbscan_eps_value=1.0,dbscan_min_samples=3,display_process=False)
         #cluster 1
-        entity_mng.add_entity("blabla1",0,0,5,"toto")
-        entity_mng.add_entity("blabla2",0,0.5,5,"toto")
-        entity_mng.add_entity("blabla3",0.5,0,5,"toto")
-        entity_mng.add_entity("blabla4",0.5,0,4.3,"toto")
+        entity_mng.add_entity(Entity("blabla1"),0,0,5,"toto")
+        entity_mng.add_entity(Entity("blabla2"),0,0.5,5,"toto")
+        entity_mng.add_entity(Entity("blabla3"),0.5,0,5,"toto")
+        entity_mng.add_entity(Entity("blabla4"),0.5,0,4.3,"toto")
 
-        entity_mng.add_entity("blabla5",3,2,0,"car")
-        entity_mng.add_entity("blabla6",2.5,1.5,0,"car")
-        entity_mng.add_entity("blabla7",3.2,1.75,0,"boat")
+        entity_mng.add_entity(Entity("blabla5"),3,2,0,"car")
+        entity_mng.add_entity(Entity("blabla6"),2.5,1.5,0,"car")
+        entity_mng.add_entity(Entity("blabla7"),3.2,1.75,0,"boat")
         
         #cluster 3
-        entity_mng.add_entity("blabla8",10,0.3,5,"bottle")
-        entity_mng.add_entity("blabla9",9.7,0,5,"bottle")
-        entity_mng.add_entity("blabla10",10.3,0.3,5,"bottle")
+        entity_mng.add_entity(Entity("blabla8"),10,0.3,5,"bottle")
+        entity_mng.add_entity(Entity("blabla9"),9.7,0,5,"bottle")
+        entity_mng.add_entity(Entity("blabla10"),10.3,0.3,5,"bottle")
 
-        clusters=entity_mng._process_buffer()
+        clusters=entity_mng.process_buffer()
         self.assertEqual(2,len(clusters))
 
-    def test_check_and_process_buffer_size_not_enought(self):
-        entity_mng = EntityMergeMng(5,5,dbscan_eps_value=1.0,dbscan_min_samples=3)
-        #cluster 1
-        entity_mng.add_entity("blabla1",0,0,5,"car")
-        entity_mng.add_entity("blabla2",0,0.5,5,"car")
-        entity_mng.add_entity("blabla3",0.5,0,5,"car")
-        entity_mng.add_entity("blabla4",0.5,0,4.3,"car")
-        #entity_mng.add_entity("blabla5",3,5,5)
-
-        clusters = entity_mng.check_and_process_buffer()
-        self.assertEqual(0,len(clusters))
 
     def test_check_and_process_buffer_size_ok(self):
         entity_mng = EntityMergeMng(5,5,dbscan_eps_value=1.0,dbscan_min_samples=3)
         #cluster 1
-        entity_mng.add_entity("blabla1",0,0,5,"car")
-        entity_mng.add_entity("blabla2",0,0.5,5,"car")
-        entity_mng.add_entity("blabla3",0.5,0,5,"car")
-        entity_mng.add_entity("blabla4",0.5,0,4.3,"car")
-        entity_mng.add_entity("blabla5",3,5,5,"car")
+        entity_mng.add_entity(Entity("blabla1"),0,0,5,"car")
+        entity_mng.add_entity(Entity("blabla2"),0,0.5,5,"car")
+        entity_mng.add_entity(Entity("blabla3"),0.5,0,5,"car")
+        entity_mng.add_entity(Entity("blabla4"),0.5,0,4.3,"car")
+        entity_mng.add_entity(Entity("blabla5"),3,5,5,"car")
 
-        clusters = entity_mng.check_and_process_buffer()
+        clusters = entity_mng.process_buffer()
         self.assertEqual(1,len(clusters))
 
-    def test_check_and_process_buffer_ttl_ok(self):
-        entity_mng = EntityMergeMng(5,5,dbscan_eps_value=1.0,dbscan_min_samples=2)
-        #cluster 1
-        entity_mng.add_entity("blabla1",0,0,5,"car")
-        entity_mng.add_entity("blabla2",0,0.5,5,"car")
-        entity_mng.add_entity("blabla3",0.5,0,5,"car")
-        #entity_mng.add_entity("blabla5",3,5,5)
-        time.sleep(6)
+    #def test_check_and_process_buffer_ttl_ok(self):
+    #    entity_mng = EntityMergeMng(5,5,dbscan_eps_value=1.0,dbscan_min_samples=2)
+    #    #cluster 1
+    #    entity_mng.add_entity(Entity("blabla1"),0,0,5,"car")
+    #    entity_mng.add_entity(Entity("blabla2"),0,0.5,5,"car")
+    #    entity_mng.add_entity(Entity("blabla3"),0.5,0,5,"car")
+    #    #entity_mng.add_entity("blabla5",3,5,5)
+    #    time.sleep(6)
+#
+    #    clusters = entity_mng.process_buffer()
+    #    self.assertEqual(1,len(clusters))
 
-        clusters = entity_mng.check_and_process_buffer()
-        self.assertEqual(1,len(clusters))
-
+    def test_check_clustering_with_large_different_categories(self):
+        list=[['0.478925979158', '1.72041366053', '0.338162953405', 'pitcher'], ['0.0', '0.0', '0.0', 'legoduplo'], ['0.839994215872', '1.39460499135', '0.220023868548', 'pitcher'], ['1.43720197384', '1.74155567275', '0.515741799004', 'pitcher'], ['0.0', '0.0', '0.0', 'legoduplo'], ['3.10619888936', '3.18450739502', '0.000960778115468', 'coloredwoodblock'], ['3.13818368201', '3.49937216121', '0.000840265950091', 'coloredwoodblock'], ['1.83351236984', '5.08687828065', '0.861111093251', 'pottedmeat'], ['1.51749251286', '4.82177696414', '0.748751834135', 'sugar'], ['1.13746645355', '5.0060511129', '0.895427254161', 'pottedmeat'], ['0.858438474374', '4.62266843231', '0.961419217456', 'mustard'], ['0.691892589903', '4.95993932903', '0.862440519674', 'pottedmeat'], ['0.406098451559', '4.49834130458', '0.895938725193', 'mustard'], ['0.12812369009', '3.89425594954', '0.534817657272', 'legoduplo'], ['-0.27551997642', '4.73159381279', '0.600139205634', 'gelatin'], ['-0.583274241587', '4.63919945311', '0.600312986393', 'pottedmeat'], ['-0.725278029395', '4.54877949488', '0.530165151059', 'mustard'], ['-0.930979771043', '4.17856720669', '0.298943007401', 'lemon'], ['0.0', '0.0', '0.0', 'gelatin'], ['0.0', '0.0', '0.0', 'pottedmeat'], ['0.324886017928', '4.84101028613', '0.482104467322', 'tuna'], ['0.0', '0.0', '0.0', 'mustard'], ['-0.15773771022', '4.70698538593', '0.121250146812', 'strawberry'], ['-0.512578612166', '4.60848387188', '0.399463721501', 'lemon'], ['0.0', '0.0', '0.0', 'mustard'], ['0.113144736685', '4.78243481513', '0.233974019509', 'legoduplo'], ['-0.327620506584', '4.66011023016', '0.0769996761656', 'legoduplo'], ['0.438153607422', '4.8728203368', '0.347743386239', 'legoduplo'], ['-0.27024133607', '5.62588573594', '0.181816546111', 'legoduplo'], ['0.3229969383', '4.84099333885', '0.350572543268', 'legoduplo'], ['-0.655597595775', '3.44368888534', '0.390338001539', 'pitcher'], ['-0.240414552757', '5.08268149248', '0.311368182172', 'pitcher'], ['2.66803955614', '4.44257795974', '0.500825322086', 'legoduplo'], ['3.61838012352', '4.3264846375', '0.4755975006', 'cup'], ['3.62616664945', '3.72937549471', '0.000888229785055', 'legoduplo']]
+        entity_mng = EntityMergeMng(100,100,dbscan_eps_value=0.5,dbscan_min_samples=2,display_process=True)
+        i=0
+        for elt in list:
+            entity_mng.add_entity(Entity(elt[3]+str(i),label=elt[3]),elt[0],elt[1],elt[2],elt[3])
+            i=i+1
+        clusters = entity_mng.process_buffer()
+        self.assertTrue(True)
+    
 
 if __name__ == '__main__':
+    rospy.init_node('namo_scan', anonymous=True)
     unittest.main()
